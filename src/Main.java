1b1f0fef28b8cb0734107548c15ef2d511a639d9
Guschin Evgeny <eg@vidis.tech>
2023-09-18 16:56:15 +0300

    Add condition operators

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         int accBal = 100; // initial account balance
-        int chargeAccBal = 300; // charge account balance first subscriber #1
-        int chargeAccBalCl2 = 1100; // charge account balance first subscriber #2
-        boolean cshbkCrd = chargeAccBal == 1000; // comparison of account charging subscriber #1
-        int cashback1 = ((chargeAccBal / 1000) * 10); // rules of cashback
-        System.out.println(" вы пополнили счет " + chargeAccBal);
-        System.out.println(" кэшбэк составил " + cashback1);
-        System.out.println(" теперь ваш баланс с учетом кэшбэка равен " + ((chargeAccBal + accBal) + cashback1));
-        System.out.println();
-        boolean cshbkCrd2 = chargeAccBalCl2 == 1000; // comparison of account charging subscriber #2
-        int cashback2 = ((chargeAccBalCl2 / 100)); // condition must be added. If cshbk_crd2 = true do {...}
-        System.out.println(" вы пополнили счет " + chargeAccBalCl2);
-        System.out.println(" кэшбэк составил " + cashback2);
-        System.out.println(" теперь ваш баланс с учетом кэшбэка равен " + (chargeAccBalCl2 + accBal + cashback2));
+        int chargeAccBal = 1100; // charge account balance  subscriber
+        boolean a = ((chargeAccBal > 1000)); // comparison of account charging subscriber
+        if (a) {
+            int cashbk1 = ((chargeAccBal / 100)); // rules of cashback
+            System.out.println(" вы пополнили счет " + chargeAccBal);
+            System.out.println(" кэшбэк составил " + cashbk1);
+            System.out.println(" теперь ваш баланс с учетом кэшбэка равен " + ((chargeAccBal + accBal) + cashbk1));
+            System.out.println();
+        } else {
+            System.out.println(" вы пополнили счет " + chargeAccBal);
+            System.out.println(" кэшбэк не начисляется ");
+            System.out.println(" теперь ваш баланс  равен " + ((chargeAccBal + accBal)));
+            System.out.println();
+        }
     }
 }
